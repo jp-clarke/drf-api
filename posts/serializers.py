332d567c9c0a2e8e3ff955fae991355db3d470e2
John Clarke <jp_clarke@outlook.com>
2023-09-08 17:50:22 +0000

    Add PostList view

--- posts/serializers.py
+++ posts/serializers.py
@@ -30,6 +30,6 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'profile_id', 'profile_image', 'created_at',
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at',
             'updated_at', 'title', 'content', 'image', 'image_filter'
         ]
