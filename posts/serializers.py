5e09d733cfa97158bb268e0a7a4b0edbfc0b72f5
John Clarke <jp_clarke@outlook.com>
2023-09-09 09:41:18 +0000

    CommentSerializer Challenge

--- posts/serializers.py
+++ posts/serializers.py
@@ -30,6 +30,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at',
-            'updated_at', 'title', 'content', 'image', 'image_filter'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'created_at', 'updated_at', 'title', 'content', 'image',
+            'image_filter'
         ]
