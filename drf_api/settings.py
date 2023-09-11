e16ba22d78d38108e219fe797ad39a1d4d90d938
John Clarke <jp_clarke@outlook.com>
2023-09-11 21:34:43 +0000

    Update secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -55,14 +55,14 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-jpclarke-drfapi-rr3lwaykamw.ws-eu104.gitpod.io',
-    'drf-api-jc-72db7ee88def.herokuapp.com/',
+    'drf-api-jc-72db7ee88def.herokuapp.com/'
 ]
 
 
