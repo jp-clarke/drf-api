36ec04a8da0a7530a030f49b11d57f0509c18db4
John Clarke <jp_clarke@outlook.com>
2023-09-12 19:19:31 +0000

    Set Debug to True

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,7 +58,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+# DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-jpclarke-drfapi-rr3lwaykamw.ws-eu104.gitpod.io',
