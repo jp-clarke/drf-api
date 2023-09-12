965c5b4c63be17bb704a6e0c293e590232a3a279
John Clarke <jp_clarke@outlook.com>
2023-09-12 19:53:36 +0000

    dj-rest-auth Bug Fix and revert Debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,8 +58,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-jpclarke-drfapi-rr3lwaykamw.ws-eu104.gitpod.io',
